2eb57064ad86812848a2bcdf7419c9b18682e91e
Likuniabikunia <acc4you.practice@gmail.com>
2024-01-20 17:10:07 +0400

    first commit

---
+++ day1/day1.py
@@ -0,0 +1,8 @@
+name = "lika"
+surname = "gigiashvili"
+age = 15 
+height = 1.55                                                                                                                                                                                           
+print(name + " " + (surname) + " " + ("aris") + " " + str(age) + " " + ("wlis"))
+print(name + " " + ("aris") + " " + str(height) + " " + ("metri"))
+print(name +" " + ("swavlobs") + " " + ("goa") + " " + ("shi"))
+
