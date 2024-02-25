cc987f2683e429d583b3aef2c61b4dae66ad4815
Likuniabikunia <acc4you.practice@gmail.com>
2024-02-26 01:06:10 +0400

    'goa'

--- Day 07/homeworks/lesson7.py
+++ Day 07/homeworks/lesson7.py
@@ -1,18 +1,19 @@
 # for i in range(0,10,2):
-#     print(i)
+#      print(i)
 
 
 # counter = 0
 # while counter < 20:
-#     print(counter)
-#     counter += 1
+#      print(counter)
+#      counter += 1
 
-
-# sum = 1
-# while sum < 5:
-#     sum += sum
+# sum = 0
+# for i in range(6):
+#   sum += i
 # print(sum)
 
-# text = "mate sauketeso lideri"
+# text = "mate best leader"
 # for char in text:
 #     print(char)
+
+
