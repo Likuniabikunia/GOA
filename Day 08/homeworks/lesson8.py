a9bb9ef759421679f5cb657e357d1e9907ab7f4f
Likuniabikunia <acc4you.practice@gmail.com>
2024-03-01 23:24:51 +0400

    'goa'

---
+++ Day 08/homeworks/lesson8.py
@@ -0,0 +1,25 @@
+name=input("enter your name: ")
+email=input("enter your email: ")
+if email == "likunna12@gmail.com":
+    print("successfull")
+else:
+    print("unssuccesfull")
+
+age=int(input("enter your age: "))
+
+if age >= 18:
+    print("you are good to go")
+
+if age < 18:
+    print("you are underage")
+
+
+item =input("enter your item: ")
+if item == "YSL handbag":
+    print("successfully added to cart xoxo")
+else:
+    print("out of stock")
+
+
+
+    
