3d697fc9bedc1bd9e8d2f4480dce34e6d7c231c9
Likuniabikunia <acc4you.practice@gmail.com>
2024-03-06 19:05:40 +0400

    'goa'

---
+++ Day 09/homeworks/lesson9.py
@@ -0,0 +1,73 @@
+#დავალება პირველი-შეამოწმეთ იყოფა თუ არა მომხმარებლის მიერ შემოტანილი რიცხვი
+#2-ზე და 4-ზე ზუსტად input ფუნქციის მეშვეობით
+
+# num=int(input("enter your number: "))
+# print(num % 2 == 0 and num % 4 == 0)
+
+
+#დავალება მე-2
+
+#greater than operator (>)
+
+# print(10 > 5) #True
+
+# print(30 > 0) #True
+
+
+#less than operator
+
+# print(20 < 40) #True
+
+# print(50 < 100) #True
+
+
+#greater than or equal to operator (>=)
+
+# print(100 >= 100) #True
+
+# print(6 >= 4) #True
+
+
+#less than or equal to operator (<=)
+
+# print(30 <= 30) True
+
+# print(4 <= 8) #True
+
+
+
+#equal to operator (==)
+
+# print(60 == 60) #True
+
+
+
+#does not equal to operator (!=)
+
+# print(10 != 9) #True
+
+
+
+#დავალება მე-3
+
+num = int(input("enter num: "))
+# print(num < 5 and num > 7)
+# print(num < 10 and num > 3)
+# print(num >= 5 and num != 10)
+# print(num == 5 and num > 4)
+# print(num > 6 and num < 9)
+# print(num < 90 and num > 70)
+# print(num > 33 and num < 40)
+# print(num < 5 and num > 2)
+# print(num > 67 and num < 100)
+# print(num > 7 and num > 2)
+
+
+
+
+
+
+
+
+
+
