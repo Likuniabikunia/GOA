5b8ccecf937b6e45c300591a9a0b8886be6a32f7
Likuniabikunia <acc4you.practice@gmail.com>
2024-03-08 21:54:02 +0400

    'goa'

---
+++ Day 10/homeworks/1lesson10.py
@@ -0,0 +1,53 @@
+#დავალება 1
+#შეიტანეთ 5 პროგრამირების ენა სიაში,ჯერ დავპრინტოთ მთლიანი სია,შემდეგ კი მხოლოდ ერთ-ერთი
+
+
+# programming_languages_list = ["Python", "JavaScript", "HTML", "CSS", "Ruby"]
+# print(programming_languages_list)
+# print(programming_languages_list[3])
+
+
+#დავალება 2
+#შექმენით სია (string, int, float, bool) საბოლოოდ ყველა ელემენტი ცალკ-ცალკე გამოიტანეთ
+
+# datatypes_list = ["hello", 5, 5.2, 10 > 3]
+# print(datatypes_list[0])
+# print(datatypes_list[1])
+# print(datatypes_list[2])
+# print(datatypes_list[3])
+
+
+#დავალება 3
+#შექმენით სია სადან 0-დან 20-ის ჩათვლით გვექნება 4-ის ჯერადები.დაპრინტეთ უდიდესი ჯერადი
+ 
+# numbers_list = []
+# for i in range(0,20 + 1):
+#     if i % 4 == 0:
+#         numbers_list.append(i)
+# print(numbers_list)
+# print(max(numbers_list))
+
+
+#დავალება 4
+#შექმენით სია 30-დან 50-ის ჩათვლით კენტი რიცხვები.დაპრინტეთ სამი უმცირესი ელემენტის ჯამი
+#+ დაპრინტეთ სამი უმცირესი ელემენტის ჯამი
+
+
+# num_list = []
+# for i in range(30, 50+1):
+#     if i % 2 == 1:
+#         num_list.append(i)
+# print(num_list)
+# print(num_list[0] + num_list[1] + num_list[2])
+
+
+nums = [1, 5, 4, 8]
+print(nums[0] + nums[2])
+
+
+
+
+
+
+
+
---
+++ Day 10/homeworks/classwork.py
@@ -0,0 +1,21 @@
+# age = int(input("enter your age: "))
+# if age >= 0 and age < 13:
+#     print("you are underage")
+# elif age < 18 and age >= 13:
+#     print("you are a teenager")
+# elif age >= 18 and age < 60:
+#     print("you are an adult")
+# else:
+#     print("you are old")
+
+
+#list
+
+# shopping_list = ["Aplle", "5", "Woods", "true" ]
+# print(shopping_list[3])
+
+
+
+movie_list = ["Flipped", "Aquaman", "Twighlight", "Weathering with you", "Orphan"]
+print(movie_list)
+print(movie_list[0])
