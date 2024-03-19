b2fcf499c0bf498b5b3db60a6e7be7473ff781a7
Likuniabikunia <acc4you.practice@gmail.com>
2024-03-20 01:43:04 +0400

    'goa'

---
+++ Day 13/homeworks/lesson13.py
@@ -0,0 +1,107 @@
+#დავალება 1-ჯგუფი 15
+#შექმენით სია რომელსაც გადასცემთ რამდენიმე ელემენტს 
+#შემდეგ for ციკლის გამოყენებით დაპრინტეთ ამ სიის ელემენტები სათითაოდ.
+
+# cart = ["concealer", "bronzer", "highlighter", "setting spray"]
+# for i in cart:
+#     print(i)
+
+
+
+
+
+#ჯგუფი 11
+#დავალება 1
+#print numbers for 1 to 10 using a while loop
+
+
+# i = 1
+# while i <= 10:
+#     print(i)
+#     i = i + 1
+
+
+#დავალება 2
+#write a program that prints even numbers from 1 to 10 using a for loop
+
+#example N1
+
+# for i in range(2,10,2):
+#     print(i)
+
+#example N2
+    
+# for i in range(1,10 + 1):
+#     if i % 2 == 0:
+#         print(i)
+
+
+#დავალება 3
+#Write a program that asks the user to enter a number 
+#then prints whether the number is positive, negative, or zero using an if-else statement
+
+# num = int(input("enter a number: "))
+# if num > 0:
+#     print("number is positive")
+# elif num < 0:
+#     print("number is negative")
+# else:
+#     if num == 0:
+#         print("number is zero")
+
+
+#დავალება 4
+#Write a program that asks the user to enter a password.
+#If the password is "abc123", print "Access granted"; otherwise, print "Access denied".
+
+# password = input("enter your password: ")
+# if password == "abc123":
+#     print("Access granted")
+# else:
+#     print("Access denied")
+
+
+
+#დავალება 5
+#Write a program that prints numbers from 1 to 10, 
+#but stops if the number is 5 using a while loop and the break statement. break
+
+# i = 1
+# while i < 10:
+#     print(i)
+#     if i == 5:
+#         break 
+#     i += 1
+
+
+#დავალება 6
+#Write a program that asks the user to enter a number between 1 and 5. If the number is less than
+#1 or greater than 5, 
+#print "Invalid input". If the number is between 1 and 5, print "Valid input".
+
+# num = int(input("enter a number between 1 to 5: "))
+# if num < 1:        
+#     print("invalid input")
+# elif num > 5:
+#     print("invalid input")    
+# else:
+#     print("valid input")
+
+
+#დავალება 7
+#Write a program that asks the user to enter a number.
+#If the number is divisible by 3, print "Fizz". If the number is divisible by 5, print "Buzz". 
+#If the number is divisible by both 3 and 5, print "FizzBuzz". Otherwise, print the number itself.
+
+# num = int(input("enter a num: "))
+# if num % 3 == 0 and num % 5 == 0:
+#     print("fizzbuzz")
+# elif num % 3 == 0:
+#     print("fizz")           
+# elif num % 5 == 0:
+#     print("buzz")
+# else:
+#     print(num)
+    
+
+    
