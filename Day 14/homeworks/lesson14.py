0921f1f01f84cc6638da40a9c43f784670d65686
Likuniabikunia <acc4you.practice@gmail.com>
2024-03-22 01:14:59 +0400

    'goa'

---
+++ Day 14/homeworks/lesson14.py
@@ -0,0 +1,90 @@
+#დავალება 1
+#შექმენით სია რომელშიც შეიტანთ რიცხვებს 0-დან 20-მდე ციკლის გარეშე
+#ბოლოს დაპრინტეთ მთლიანი სია
+
+# num_list = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+# print(num_list)
+
+#დავალება 2
+#შექმენით ახალი სია რომელშიც შეიტანთ ლუწ რიცხვებს წინა სიიდან
+#ბოლოს დაპრინტეთ ეს სია
+
+# even_num_list = [2,4,6,8,10,12,14,18,20]
+# print(even_num_list)
+
+
+#დავალება 3
+#შექმენით ახალი სია, შემდეგ 50-იდან 100-მდე ამ სიაში შეიტანეთ ყველა რიცხვი 
+#დასერჩეთ python array append).
+#ბოლოს დაპტინტეთ თქვენთვის სასურველი სიის ნაწილი უარყოფითი index-ების საშუალებით
+
+# num = []
+
+# for i in range(50, 101):
+#     num.append(i)
+# print(num[30:-2])
+
+
+#დავალება 4
+#Ask user for two inputs and store them as variables, their type has to be int. 
+#Then use for loop, use lower number from this two variables as start, Higher number as end, 
+#you do not need step. 
+#After that, you'll have to use if statement to only print multiples of five.
+
+# num1 = int(input("enter the first number: "))
+# num2 = int(input("enter the second number: "))
+
+# nums_list = []
+# for i in range(min(num1, num2), max(num1, num2)):
+#     nums_list.append(i)
+# for num1 in nums_list:
+#     if num1 % 5 == 0:
+#         print(num1)
+
+
+#დავალება 5
+
+#დავალება5) შექმენით ახალი სია. შემდეგ მომხმარებელს შეეკითხეთ მისი ასაკი და თუ ასაკი 18-ზე მეტი იქნება,
+#შეეკითხეთ მას სახელი. მეორე ინფუთის შემდეგ სახელი შეიტანეთ სიაში და დაპრინტეთ ის
+
+
+# user_info = []
+
+# user_age = int(input("how old are you?: "))
+
+# if user_age > 18:
+#     user_name = input("whats your name?: ")
+#     user_info.append(user_age)
+#     user_info.append(user_name)
+# print(user_info)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
