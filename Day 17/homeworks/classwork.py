e6264102fb9bada3cfd890b87328df907c3119a5
Likuniabikunia <acc4you.practice@gmail.com>
2024-03-30 01:22:08 +0400

    'goa'

---
+++ Day 17/homeworks/classwork.py
@@ -0,0 +1,38 @@
+# def func():
+#     for i in range(10):
+#         print(i + 2)
+
+# func()
+# func()
+# func()
+# func()
+
+
+
+
+# def welcome(name, lastname):
+    # print("welcome" + " " + name + " " + lastname)
+
+# welcome("sidug", "oasudh")
+# welcome("aoz[isedfn", "aoiefj")
+# welcome("osdjfvb", "isufdbh")
+
+
+
+
+
+# def iteration(num):
+#     for i in range(num):
+#         print(i)
+
+# iteration(8)
+
+
+
+
+
+# def func(count):
+#     for count in range(count):
+#         print("lika")
+
+# func(8)
---
+++ Day 17/homeworks/lesson17.py
@@ -0,0 +1,88 @@
+#write a function that takes a list of numbers as input and returns the sum of all the numbers in the list.
+
+# def sum_of_numbers_in_list(num):
+#     total = 0
+#     for i in num:
+#         total = total + i
+#     return total 
+# num = [98,8,1,5,6,18]
+# print(sum_of_numbers_in_list(num))
+
+
+#Write a function that takes a list of strings as input 
+#and returns a new list containing only the strings that have a length greater than 5.
+
+# def strings_in_list(word):
+#     result = []
+#     for i in word:
+#         if len(i) > 5:
+#             result.append(i)
+#         return result
+# word = ["sunset", "radioactive", "hello"]
+# print(strings_in_list(word))
+
+
+
+#Write a function that takes a list of numbers as input and returns a new list 
+#containing only the even numbers from the original list.
+
+# def even_numbers(num):
+#     even_numbers_list = []
+    
+#     for num in num:
+#         if num % 2 == 0:
+#             even_numbers_list.append(num)
+#     return even_numbers_list    
+
+# print(even_numbers([67,83,29,22,25]))
+
+
+
+
+#Write a function that takes a list of numbers as input and returns the largest number in the list.
+
+
+# def largest_number(num):
+#     return max(num)
+# print(largest_number([22,55,66,25,99,1020]))
+
+
+
+#VWrite a function that takes a list of strings as input 
+#and returns a new list containing only the strings that start with the letter "a"
+
+
+# def string_starting_with_A(word):
+#     list = []
+#     for i in word:
+#         if i[0] == "a":
+#             list.append(i)
+#     return list
+
+# word = ["eka", "avocado", "irakli", "anano"]
+# print(string_starting_with_A(word))
+
+
+#6
+
+
+
+
+#Write a function that takes a list of strings as input and returns a new list containing the lengths of each string.
+
+
+# def string_lenghts(word):
+#     list = []
+#     for i in word:
+#         list.append (len(i))
+
+#     return list
+
+# word = ["avocado best", "avocado for life", "avocado plant"]
+# print(string_lenghts(word))
+
+
+#8
+
+
+
